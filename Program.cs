d6ab35fb0a65f55a2225a8497165a2509a7e6a3a
Joseph Collard <jcollard@users.noreply.github.com>
2022-01-27 04:27:33 -0800

    chore: Add feedback

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PersonalProject1
 {
@@ -8,5 +9,16 @@ namespace PersonalProject1
         {
             Console.WriteLine("Hello World!");
         }
+
+        // TODO(jcollard 2022-01-27): Add documentation comment
+        public static int GetValidAnswer(List<string> answers)
+        {
+            // TODO(jcollard 2022-01-27): Add step-by-step list of 
+            // things to accomplish
+            return -1;
+        }
+
+        // TODO(jcollard 2022-01-27): 
+        // Add method stubs for remaining methods
     }
 }
